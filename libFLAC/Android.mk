ab37b6247df0e8c4ec0ccaa870e667f62c74975b
Chad Brubaker <cbrubaker@google.com>
2013-05-17 18:16:30 -0700

    Add signed integer overflow checking to flac.
    Migrate flac over to Clang in order to use its -ftrapv option to abort
    on signed overflow.
    
    Change-Id: I9f92459b6b5ecfdc49377a191db8d0265f190ab1

--- libFLAC/Android.mk
+++ libFLAC/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
+LOCAL_CLANG := true
 LOCAL_SRC_FILES:= \
 	bitmath.c \
 	bitreader.c \
@@ -17,7 +17,6 @@ LOCAL_SRC_FILES:= \
 	stream_encoder.c \
 	stream_encoder_framing.c \
 	window.c
-
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)/.. \
@@ -26,6 +25,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_CFLAGS += -DHAVE_CONFIG_H -DFLAC__NO_MD5 -DFLAC__INTEGER_ONLY_LIBRARY
 LOCAL_CFLAGS += -D_REENTRANT -DPIC -DU_COMMON_IMPLEMENTATION -fPIC
 LOCAL_CFLAGS += -O3 -funroll-loops -finline-functions
+LOCAL_CFLAGS += -ftrapv
 
 LOCAL_LDLIBS += -lm
 
